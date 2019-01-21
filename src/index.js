122159e1d1c37e6528d3b5065c71aaea9a87132e
quesurifn <kyle.c.r.fahey@gmail.com>
2019-01-21 15:31:56 -0600

    Register service worker

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom'
 import {App} from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
 ReactDOM.render(
 <BrowserRouter>
     <App />
 </BrowserRouter>
 , document.getElementById('root'));
-registerServiceWorker();
+unregister();
